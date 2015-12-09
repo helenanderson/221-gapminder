bed9cf53275bcb73c6a6116d46e0a9b06a39bf9f
helenanderson <helenanderson@stanford.edu>
2015-12-09 10:13:27 -0800

    bayesian2 contains the continent code

--- bayesian2.py
+++ bayesian2.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 import numpy as np
 from sklearn import linear_model
@@ -9,16 +9,16 @@ from sklearn.externals import joblib
 from sklearn import preprocessing
 from sklearn.metrics import mean_absolute_error, mean_squared_error, median_absolute_error
 import os, pickle, operator
+import random
 
 continents = {
-  "Africa": ["Algeria","Angola","Benin","Botswana","Burkina","Burundi","Cameroon","Cape Verde","Central African Republic","Chad","Comoros","Congo","Congo, Rep.","Djibouti","Egypt","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Cote d'Ivoire","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"],
-  "Asia": ["Afghanistan","Bahrain","Bangladesh","Bhutan","Brunei","Myanmar","Cambodia","China","East Timor","India","Indonesia","Iran","Iraq","Israel","Japan","Jordan","Kazakhstan","North Korea","South Korea","Kuwait","Kyrgyz Republic","Lao","Lebanon","Malaysia","Maldives","Mongolia","Nepal","Oman","Pakistan","Philippines","Qatar","Russia","Saudi Arabia","Singapore","Sri Lanka","Syria","Tajikistan","Thailand","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen"],
-  "Europe": ["Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","Netherlands","Norway","Poland","Portugal","Romania","San Marino","Serbia","Slovak Republic","Slovenia","Spain","Sweden","Switzerland","Ukraine","United Kingdom","Vatican City"],
-  "North America": ["Antigua and Barbuda","Bahamas","Barbados","Belize","Canada","Costa Rica","Cuba","Dominica","Dominican Republic","El Salvador","Grenada","Guatemala","Haiti","Honduras","Jamaica","Mexico","Nicaragua","Panama","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","Trinidad and Tobago","United States of America"],
-  "Oceania": ["Australia","Fiji","Kiribati","Marshall Islands","Micronesia","Nauru","New Zealand","Palau","Papua New Guinea","Samoa","Solomon Islands","Tonga","Tuvalu","Vanuatu"],
-  "South America": ["Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela"]
+  "Africa": ["Cote D'Ivoire","Mayotte",u"São Tomé and Príncipe","Congo [Republic]","Congo [DRC]",u"Côte d'Ivoire","Western Sahara","Reunion","Central African Rep.","Congo, Dem. Rep.","Algeria","Angola","Benin","Botswana","Burkina","Burkina Faso","Burundi","Cameroon","Cape Verde","Central African Republic","Chad","Comoros","Congo","Congo, Rep.","Djibouti","Egypt","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Cote d'Ivoire","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"],
+  "Asia": ["Myanmar [Burma]","Democratic Republic of Timor-Leste","Macao, China","Laos","Libyan Arab Jamahiriya","Korea, Dem. Rep.","Yemen, Rep.","Hong Kong, China","Syrian Arab Republic","Korea, Rep.","West Bank and Gaza","Brunei Darussalam","Timor-Leste","Viet Nam","Taiwan","Afghanistan","Bahrain","Bangladesh","Bhutan","Brunei","Myanmar","Cambodia","China","East Timor","India","Indonesia","Iran","Iraq","Israel","Japan","Jordan","Kazakhstan","North Korea","South Korea","Kuwait","Kyrgyz Republic","Kyrgyzstan","Lao","Lebanon","Malaysia","Maldives","Mongolia","Nepal","Oman","Pakistan","Philippines","Qatar","Russia","Saudi Arabia","Singapore","Sri Lanka","Syria","Tajikistan","Thailand","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen"],
+  "Europe": ["Slovak republic","USSR","West Germany","East Germany","Gibraltar","Macedonia [FYROM]","Slovakia","Czech Rep.","Yugoslavia","Czechoslovakia","Serbia and Montenegro","Channel Islands",u"Åland","Greenland","Macedonia, FYR","Russian Federation","Serbia and Montenegro","Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","Netherlands","Norway","Poland","Portugal","Romania","San Marino","Serbia","Slovak Republic","Slovenia","Spain","Sweden","Switzerland","Ukraine","United Kingdom","Vatican City"],
+  "North America": ["Aruba","Virgin Islands (U.S.)","Netherlands Antilles","Guadeloupe","Martinique","St. Kitts and Nevis","Bermuda","British Virgin Islands","Cayman Islands","St. Vincent and the Grenadines","Turks and Caicos Islands","St. Lucia","Dominican Rep.","Anguilla","Montserrat","Puerto Rico","Antigua and Barbuda","Bahamas","Barbados","Belize","Canada","Costa Rica","Cuba","Dominica","Dominican Republic","El Salvador","Grenada","Guatemala","Haiti","Honduras","Jamaica","Mexico","Nicaragua","Panama","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","Trinidad and Tobago","United States of America","United States"],
+  "Oceania": ["Guam","New Caledonia","Northern Mariana Islands","Tokelau","Cook Islands","Wallis et Futuna","Micronesia, Fed. Sts.","American Samoa","Cook Is","French Polynesia","Niue","Australia","Fiji","Kiribati","Marshall Islands","Micronesia","Nauru","New Zealand","Palau","Papua New Guinea","Samoa","Solomon Islands","Tonga","Tuvalu","Vanuatu"],
+  "South America": ["French Guiana","Venezuela (Bolivarian Republic of)","Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela"]
 }
-
 
 def getRegion(countryName):
   for region in continents:
@@ -93,15 +93,13 @@ def create_feature_vectors():
       if feature_vectors.get(pair):
         if prevVal != None:
             feature_vectors[pair][0]["HIV " + str(years_ago) + " years ago"] = prevVal
-
   return feature_vectors
 
 #feature_vectors = create_feature_vectors()
 #pickle.dump(feature_vectors, open('featureVectors.p', 'wb'))
 feature_vectors = {}
 with open('imputed_feature_vectors.p') as data_file:    
-       feature_vectors = pickle.load(data_file)
-
+  feature_vectors = pickle.load(data_file)
 
 feature_vectors_arr = []
 hiv_arr = []
@@ -167,8 +165,8 @@ predictions = clf.predict(test_data)
 # print clf.coef_
 weights = []
 for i, coef in enumerate(clf.coef_):
-  #if not "Country" in feature_names[i]:
-  weights.append((feature_names[i], coef))
+  if not "Country" in feature_names[i]:
+    weights.append((feature_names[i], coef))
 weights.sort(key=lambda tup: abs(tup[1]))
 print weights
 
@@ -207,11 +205,9 @@ from libpgm.pgmlearner import PGMLearner
 # featureVectorSamples = [v[0] for v in feature_vectors.values()]
 # featureVectorHIV = [v[1] for v in feature_vectors.values()]
 
-
 # feature_vectors_arr = []
 # with open('imputed_feature_vectors.p') as data_file:    
 #     feature_vectors_arr = pickle.load(data_file)
-
 
 bayes_partition = partition = int(len(feature_vectors_arr) * .7)
 training_arr = feature_vectors_arr[:partition]
@@ -223,21 +219,14 @@ print "%d elems in training_arr out of %d total" % (len(training_arr), len(featu
 
 vertexMap = {}
 
-  # del sample['Country'] # This doesn't work well with discrete (and irrelevant) features like names
-  # del sample['Year'] # This doesn't work well with discrete (and irrelevant) features like names
-
-  # if feature_vectors_arr[i] != None:
-
-
 # Imputing missing values by averaging those of other countries.
 # Going forward, we should totally use the linear regression code from above for it!
 condensed_feature_vectors_by_region = {}
-mainFeatures = ['HIV 10 years ago', 'indicator total health expenditure perc of GDP.xlsx', 'Indicator_BMI male ASM.xlsx', 'indicator food_consumption.xlsx', 'indicator_estimated incidence infectious tb per 100000.xlsx', 'indicator life_expectancy_at_birth.xlsx', 'indicator gapminder infant_mortality.xlsx']
+mainFeatures = ['indicator total health expenditure perc of GDP.xlsx', 'Indicator_BMI male ASM.xlsx', 'indicator food_consumption.xlsx', 'indicator_estimated incidence infectious tb per 100000.xlsx', 'indicator life_expectancy_at_birth.xlsx', 'indicator gapminder infant_mortality.xlsx']
 vertices = set(mainFeatures)
 for i, sample in enumerate(training_arr):
   newSample = {}
   newSample['HIV'] = hiv_training_arr[i]
-
   for k in sample.keys():
     if k in vertices:
       newSample[k] = sample[k]
@@ -247,11 +236,10 @@ for i, sample in enumerate(training_arr):
         vertexMap[k].append(sample)
   region = getRegion(sample['Country'])
   if not region:
-    break
+    continue
   if region not in condensed_feature_vectors_by_region:
     condensed_feature_vectors_by_region[region] = []
   condensed_feature_vectors_by_region[region].append(newSample)
-
 
 vertexAverages = {}
 for vertex, samples in vertexMap.iteritems():
@@ -260,21 +248,12 @@ for vertex, samples in vertexMap.iteritems():
     numerator += sample[vertex]
   vertexAverages[vertex] = numerator/len(samples)
 
-
 for region in condensed_feature_vectors_by_region:
   for sample in condensed_feature_vectors_by_region[region]:
     for vertex in vertices:
       if vertex not in sample.keys():
         # print "Used avg for %s" % vertex
         sample[vertex] = vertexAverages[vertex]
-
-# print condensed_feature_vectors
-# Testing just 4 vertices for now (takes a really, really long time to use all of them)
-# keysToRemove = list(vertices)[-1:]
-
-# for sample in featureVectorSamples:
-#   for key in keysToRemove:
-#     del sample[key]
 
 
 
@@ -298,20 +277,20 @@ learner = PGMLearner()
 
 # Voila, it makes us a bayesian network!
 bayesian_networks_by_region = {}
+
 for region in condensed_feature_vectors_by_region:
-  bayesian_networks_by_region[region] = learner.lg_estimatebn(condensed_feature_vectors_by_region[region])
   print region
+  bayesian_networks_by_region[region] = learner.lg_estimatebn(condensed_feature_vectors_by_region[region])
   print json.dumps(bayesian_networks_by_region[region].Vdata, indent=2)
   print json.dumps(bayesian_networks_by_region[region].E, indent=2)
 
 #Evaluation:
-predictions = []
 test_arrs_by_region = {}
 hiv_test_arrs_by_region = {}
 for i, sample in enumerate(test_arr):
   region = getRegion(sample['Country'])
-  if not region: 
-    break
+  if not region:  
+    continue
   if region not in test_arrs_by_region:   
     test_arrs_by_region[region] = []
     hiv_test_arrs_by_region[region] = []
@@ -321,6 +300,7 @@ for i, sample in enumerate(test_arr):
 
 for region in bayesian_networks_by_region:
   print region
+  print np.std(hiv_test_arrs_by_region[region])
   predictions = []
   for sample in test_arrs_by_region[region]:
     predictionSamples = bayesian_networks_by_region[region].randomsample(500, sample)
@@ -335,17 +315,16 @@ for region in bayesian_networks_by_region:
 
   predictions_std = preprocessing.scale(predictions)
 
-  # With standardized predictions, SD = 1, so we're just getting how many SDs off we are
-  test_sd = np.std(hiv_test_arrs_by_region[region])
-  print "Sd is %f" % test_sd
-  print mean_absolute_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
-  print median_absolute_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
-  print mean_squared_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
+  # # With standardized predictions, SD = 1, so we're just getting how many SDs off we are
+  # test_sd = np.std(hiv_test)
+  # print "Sd is %f" % test_sd
+  # print mean_absolute_error(hiv_test_, predictions_std) * test_sd
+  # print median_absolute_error(hiv_test, predictions_std) * test_sd
+  # print mean_squared_error(hiv_test, predictions_std) * test_sd
 
   # Without normalizing predictions, we need to use hiv_test_arr because that has the unnormalized HIV rates.  Now the error units are HIV rate percentage points.
   # These don't print on Matt's computer, for some reason... 
   print "Mean absolute error is %f" % mean_absolute_error(hiv_test_arrs_by_region[region], predictions)
   print "Median absolute error is %f" % median_absolute_error(hiv_test_arrs_by_region[region], predictions) 
   print "Mean squared error is %f" % mean_squared_error(hiv_test_arrs_by_region[region], predictions) 
-
 
--- bayesian3.py
+++ bayesian3.py
@@ -10,22 +10,6 @@ from sklearn import preprocessing
 from sklearn.metrics import mean_absolute_error, mean_squared_error, median_absolute_error
 import os, pickle, operator
 
-continents = {
-  "Africa": ["Algeria","Angola","Benin","Botswana","Burkina","Burundi","Cameroon","Cape Verde","Central African Republic","Chad","Comoros","Congo","Congo, Rep.","Djibouti","Egypt","Equatorial Guinea","Eritrea","Ethiopia","Gabon","Gambia","Ghana","Guinea","Guinea-Bissau","Cote d'Ivoire","Kenya","Lesotho","Liberia","Libya","Madagascar","Malawi","Mali","Mauritania","Mauritius","Morocco","Mozambique","Namibia","Niger","Nigeria","Rwanda","Sao Tome and Principe","Senegal","Seychelles","Sierra Leone","Somalia","South Africa","South Sudan","Sudan","Swaziland","Tanzania","Togo","Tunisia","Uganda","Zambia","Zimbabwe"],
-  "Asia": ["Afghanistan","Bahrain","Bangladesh","Bhutan","Brunei","Myanmar","Cambodia","China","East Timor","India","Indonesia","Iran","Iraq","Israel","Japan","Jordan","Kazakhstan","North Korea","South Korea","Kuwait","Kyrgyz Republic","Lao","Lebanon","Malaysia","Maldives","Mongolia","Nepal","Oman","Pakistan","Philippines","Qatar","Russia","Saudi Arabia","Singapore","Sri Lanka","Syria","Tajikistan","Thailand","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen"],
-  "Europe": ["Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Monaco","Montenegro","Netherlands","Norway","Poland","Portugal","Romania","San Marino","Serbia","Slovak Republic","Slovenia","Spain","Sweden","Switzerland","Ukraine","United Kingdom","Vatican City"],
-  "North America": ["Antigua and Barbuda","Bahamas","Barbados","Belize","Canada","Costa Rica","Cuba","Dominica","Dominican Republic","El Salvador","Grenada","Guatemala","Haiti","Honduras","Jamaica","Mexico","Nicaragua","Panama","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","Trinidad and Tobago","United States of America"],
-  "Oceania": ["Australia","Fiji","Kiribati","Marshall Islands","Micronesia","Nauru","New Zealand","Palau","Papua New Guinea","Samoa","Solomon Islands","Tonga","Tuvalu","Vanuatu"],
-  "South America": ["Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela"]
-}
-
-
-def getRegion(countryName):
-  for region in continents:
-    if countryName in continents[region]:
-      return region
-  print "MISMATCH", countryName
-
 # makes feature vectors from all .xlsx files in the working directory
 def create_feature_vectors():
   feature_vectors = {}
@@ -93,15 +77,13 @@ def create_feature_vectors():
       if feature_vectors.get(pair):
         if prevVal != None:
             feature_vectors[pair][0]["HIV " + str(years_ago) + " years ago"] = prevVal
-
   return feature_vectors
 
 #feature_vectors = create_feature_vectors()
 #pickle.dump(feature_vectors, open('featureVectors.p', 'wb'))
 feature_vectors = {}
 with open('imputed_feature_vectors.p') as data_file:    
-       feature_vectors = pickle.load(data_file)
-
+  feature_vectors = pickle.load(data_file)
 
 feature_vectors_arr = []
 hiv_arr = []
@@ -167,8 +149,8 @@ predictions = clf.predict(test_data)
 # print clf.coef_
 weights = []
 for i, coef in enumerate(clf.coef_):
-  #if not "Country" in feature_names[i]:
-  weights.append((feature_names[i], coef))
+  if not "Country" in feature_names[i]:
+    weights.append((feature_names[i], coef))
 weights.sort(key=lambda tup: abs(tup[1]))
 print weights
 
@@ -207,11 +189,9 @@ from libpgm.pgmlearner import PGMLearner
 # featureVectorSamples = [v[0] for v in feature_vectors.values()]
 # featureVectorHIV = [v[1] for v in feature_vectors.values()]
 
-
 # feature_vectors_arr = []
 # with open('imputed_feature_vectors.p') as data_file:    
 #     feature_vectors_arr = pickle.load(data_file)
-
 
 bayes_partition = partition = int(len(feature_vectors_arr) * .7)
 training_arr = feature_vectors_arr[:partition]
@@ -223,21 +203,14 @@ print "%d elems in training_arr out of %d total" % (len(training_arr), len(featu
 
 vertexMap = {}
 
-  # del sample['Country'] # This doesn't work well with discrete (and irrelevant) features like names
-  # del sample['Year'] # This doesn't work well with discrete (and irrelevant) features like names
-
-  # if feature_vectors_arr[i] != None:
-
-
 # Imputing missing values by averaging those of other countries.
 # Going forward, we should totally use the linear regression code from above for it!
-condensed_feature_vectors_by_region = {}
-mainFeatures = ['HIV 10 years ago', 'indicator total health expenditure perc of GDP.xlsx', 'Indicator_BMI male ASM.xlsx', 'indicator food_consumption.xlsx', 'indicator_estimated incidence infectious tb per 100000.xlsx', 'indicator life_expectancy_at_birth.xlsx', 'indicator gapminder infant_mortality.xlsx']
+condensed_feature_vectors =[]
+mainFeatures = ['indicator total health expenditure perc of GDP.xlsx', 'Indicator_BMI male ASM.xlsx', 'indicator food_consumption.xlsx', 'indicator_estimated incidence infectious tb per 100000.xlsx', 'indicator life_expectancy_at_birth.xlsx', 'indicator gapminder infant_mortality.xlsx']
 vertices = set(mainFeatures)
 for i, sample in enumerate(training_arr):
   newSample = {}
   newSample['HIV'] = hiv_training_arr[i]
-
   for k in sample.keys():
     if k in vertices:
       newSample[k] = sample[k]
@@ -245,13 +218,7 @@ for i, sample in enumerate(training_arr):
         vertexMap[k] = [sample]
       else:
         vertexMap[k].append(sample)
-  region = getRegion(sample['Country'])
-  if not region:
-    break
-  if region not in condensed_feature_vectors_by_region:
-    condensed_feature_vectors_by_region[region] = []
-  condensed_feature_vectors_by_region[region].append(newSample)
-
+  condensed_feature_vectors.append(newSample)
 
 vertexAverages = {}
 for vertex, samples in vertexMap.iteritems():
@@ -260,22 +227,11 @@ for vertex, samples in vertexMap.iteritems():
     numerator += sample[vertex]
   vertexAverages[vertex] = numerator/len(samples)
 
-
-for region in condensed_feature_vectors_by_region:
-  for sample in condensed_feature_vectors_by_region[region]:
-    for vertex in vertices:
-      if vertex not in sample.keys():
-        # print "Used avg for %s" % vertex
-        sample[vertex] = vertexAverages[vertex]
-
-# print condensed_feature_vectors
-# Testing just 4 vertices for now (takes a really, really long time to use all of them)
-# keysToRemove = list(vertices)[-1:]
-
-# for sample in featureVectorSamples:
-#   for key in keysToRemove:
-#     del sample[key]
-
+for sample in condensed_feature_vectors:
+  for vertex in vertices:
+    if vertex not in sample.keys():
+      # print "Used avg for %s" % vertex
+      sample[vertex] = vertexAverages[vertex]
 
 
 ############# Only temp removed! ############
@@ -297,55 +253,35 @@ for region in condensed_feature_vectors_by_region:
 learner = PGMLearner()
 
 # Voila, it makes us a bayesian network!
-bayesian_networks_by_region = {}
-for region in condensed_feature_vectors_by_region:
-  bayesian_networks_by_region[region] = learner.lg_estimatebn(condensed_feature_vectors_by_region[region])
-  print region
-  print json.dumps(bayesian_networks_by_region[region].Vdata, indent=2)
-  print json.dumps(bayesian_networks_by_region[region].E, indent=2)
+bayesianNetwork = learner.lg_estimatebn(condensed_feature_vectors)
+print json.dumps(bayesianNetwork.Vdata, indent=2)
+print json.dumps(bayesianNetwork.E, indent=2)
 
 #Evaluation:
 predictions = []
-test_arrs_by_region = {}
-hiv_test_arrs_by_region = {}
-for i, sample in enumerate(test_arr):
-  region = getRegion(sample['Country'])
-  if not region: 
-    break
-  if region not in test_arrs_by_region:   
-    test_arrs_by_region[region] = []
-    hiv_test_arrs_by_region[region] = []
-  test_arrs_by_region[region].append(sample)
-  hiv_test_arrs_by_region[region].append(hiv_test_arr[i])
+for sample in test_arr:
+  predictionSamples = bayesianNetwork.randomsample(500, sample)
+  hivPredictionSamples = [pSample['HIV'] for pSample in predictionSamples]
+  predictions.append(np.mean(hivPredictionSamples))
 
+# print len(hiv_test_arr)
+# print len(predictions)
 
-for region in bayesian_networks_by_region:
-  print region
-  predictions = []
-  for sample in test_arrs_by_region[region]:
-    predictionSamples = bayesian_networks_by_region[region].randomsample(500, sample)
-    hivPredictionSamples = [pSample['HIV'] for pSample in predictionSamples]
-    predictions.append(np.mean(hivPredictionSamples))
+result_pairs = [(predictions[i], hiv_test_arr[i]) for i in range(len(hiv_test_arr))]
+#print result_pairs
 
-  # print len(hiv_test_arr)
-  # print len(predictions)
+predictions_std = preprocessing.scale(predictions)
 
-  result_pairs = [(predictions[i], hiv_test_arrs_by_region[region][i]) for i in range(len(hiv_test_arrs_by_region[region]))]
-  #print result_pairs
+# # With standardized predictions, SD = 1, so we're just getting how many SDs off we are
+# test_sd = np.std(hiv_test)
+# print "Sd is %f" % test_sd
+# print mean_absolute_error(hiv_test_, predictions_std) * test_sd
+# print median_absolute_error(hiv_test, predictions_std) * test_sd
+# print mean_squared_error(hiv_test, predictions_std) * test_sd
 
-  predictions_std = preprocessing.scale(predictions)
-
-  # With standardized predictions, SD = 1, so we're just getting how many SDs off we are
-  test_sd = np.std(hiv_test_arrs_by_region[region])
-  print "Sd is %f" % test_sd
-  print mean_absolute_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
-  print median_absolute_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
-  print mean_squared_error(hiv_test_arrs_by_region[region], predictions_std) * test_sd
-
-  # Without normalizing predictions, we need to use hiv_test_arr because that has the unnormalized HIV rates.  Now the error units are HIV rate percentage points.
-  # These don't print on Matt's computer, for some reason... 
-  print "Mean absolute error is %f" % mean_absolute_error(hiv_test_arrs_by_region[region], predictions)
-  print "Median absolute error is %f" % median_absolute_error(hiv_test_arrs_by_region[region], predictions) 
-  print "Mean squared error is %f" % mean_squared_error(hiv_test_arrs_by_region[region], predictions) 
-
+# Without normalizing predictions, we need to use hiv_test_arr because that has the unnormalized HIV rates.  Now the error units are HIV rate percentage points.
+# These don't print on Matt's computer, for some reason... 
+print "Mean absolute error is %f" % mean_absolute_error(hiv_test_arr, predictions)
+print "Median absolute error is %f" % median_absolute_error(hiv_test_arr, predictions) 
+print "Mean squared error is %f" % mean_squared_error(hiv_test_arr, predictions) 
 
